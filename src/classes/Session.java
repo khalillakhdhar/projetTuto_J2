c008cc13688ca3068fc9afc0d23378cf2878a85b
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-23 17:00:03 +0100

    last

--- src/classes/Session.java
+++ src/classes/Session.java
@@ -1,5 +1,45 @@
 package classes;
 
 public class Session {
+private int id,duree,id_formation;
+private String date;
+private float prix;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public int getDuree() {
+	return duree;
+}
+public void setDuree(int duree) {
+	this.duree = duree;
+}
+public int getId_formation() {
+	return id_formation;
+}
+public void setId_formation(int id_formation) {
+	this.id_formation = id_formation;
+}
+public String getDate() {
+	return date;
+}
+public void setDate(String date) {
+	this.date = date;
+}
+public float getPrix() {
+	return prix;
+}
+public void setPrix(float prix) {
+	this.prix = prix;
+}
+
+
+
+
+
+
+
 
 }
