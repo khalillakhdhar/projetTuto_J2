410bc0f88f7a2da709f2beaf382f4faf73380382
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-23 16:54:45 +0100

    classes

--- src/classes/Candidat.java
+++ src/classes/Candidat.java
@@ -1,5 +1,58 @@
 package classes;
 
 public class Candidat {
+private int id;
+private String nom,prenom,tel,niveau,email,pass,grade;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public String getPrenom() {
+	return prenom;
+}
+public void setPrenom(String prenom) {
+	this.prenom = prenom;
+}
+public String getTel() {
+	return tel;
+}
+public void setTel(String tel) {
+	this.tel = tel;
+}
+public String getNiveau() {
+	return niveau;
+}
+public void setNiveau(String niveau) {
+	this.niveau = niveau;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPass() {
+	return pass;
+}
+public void setPass(String pass) {
+	this.pass = pass;
+}
+public String getGrade() {
+	return grade;
+}
+public void setGrade(String grade) {
+	this.grade = grade;
+}
+
+
+
 
 }
--- src/classes/Formation.java
+++ src/classes/Formation.java
@@ -1,5 +1,34 @@
 package classes;
 
 public class Formation {
+private int id;
+private String titre, formateur, description;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getTitre() {
+	return titre;
+}
+public void setTitre(String titre) {
+	this.titre = titre;
+}
+public String getFormateur() {
+	return formateur;
+}
+public void setFormateur(String formateur) {
+	this.formateur = formateur;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+
+
+
 
 }
