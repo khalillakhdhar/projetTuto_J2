5bc9154cf5459a4d4f572ee706cb9909e86de45d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-24 09:40:30 +0100

    connexion

--- src/classes/Connexion.java
+++ src/classes/Connexion.java
@@ -1,5 +1,22 @@
 package classes;
-
+import java.sql.DriverManager;
 public class Connexion {
-
+com.mysql.jdbc.Connection conn;
+{
+String url= "jdbc:mysql://localhost:3306/";
+String dbName;
+dbName = "formation";
+String driver= "com.mysql.jdbc.Driver";
+String userName= "root";
+String password="";
+try
+{
+Class.forName(driver).newInstance();
+conn= (com.mysql.jdbc.Connection)
+DriverManager.getConnection(url+dbName,userName, password);
+System.out.println("la connexion a reussie");
+}catch(Exception e)
+{
+System.out.println("la connexion est echoué");
 }
+}}
--- src/testes/Test.java
+++ src/testes/Test.java
@@ -1,5 +1,9 @@
 package testes;
-
+import classes.Connexion;
 public class Test {
+	public static void main(String args[])
+	{
+		Connexion c=new Connexion();
 
+	}
 }
